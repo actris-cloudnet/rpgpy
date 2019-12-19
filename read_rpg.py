7a6659a6668c78f43cf473396ffd3d5109f57efa
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-12-19 12:47:02 +0200

    loop over test files

--- read_rpg.py
+++ read_rpg.py
@@ -1,17 +1,11 @@
+import os
 import reader
 import time
-import matplotlib.pyplot as plt
 
-files = ['191108_030000_P10_ZEN.LV0',
-         '191108_100002_P10_ZEN.LV0',
-         'joyrad94_20191108120000_P01_ZEN.lv0',
-         'joyrad94_20191108000001_P01_ZEN.lv0',
-         '190823_090000_P10_ZEN.LV1']
+directory = 'data/'
 
-for file in files:
+for file in os.listdir(directory):
     start = time.time()
-    header, data = reader.read_rpg(f"data/{file}")
+    header, data = reader.read_rpg(os.path.join(directory, file))
     end = time.time()
     print(end-start)
-    plt.plot(data['EnvTemp'])
-    plt.show()
