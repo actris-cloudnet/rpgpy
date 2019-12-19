582c45e1a5efa2503f644bd35838fe01834e8cd5
Simo Tukiainen <simo.tukiainen@fmi.fi>
2019-12-19 12:50:57 +0200

    renamed the cython module

--- read_rpg.py
+++ read_rpg.py
@@ -1,11 +1,11 @@
 import os
-import reader
+import rpg_cython
 import time
 
 directory = 'data/'
 
 for file in os.listdir(directory):
     start = time.time()
-    header, data = reader.read_rpg(os.path.join(directory, file))
+    header, data = rpg_cython.read_rpg(os.path.join(directory, file))
     end = time.time()
     print(end-start)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(name='RPG Reader testing',
-      ext_modules=cythonize("reader.pyx",
+      ext_modules=cythonize("rpg_cython.pyx",
                             annotate=True,
                             language_level=3),
       )
