384f69f655188e545a4425615bb5149204fc8b77
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-01-17 09:56:11 +0200

    fixed the reader example

--- bin/read_rpg.py
+++ bin/read_rpg.py
@@ -1,11 +1,11 @@
 import os
-import rpg_cython
+import rpgpy
 import time
 
-directory = '../data/'
+directory = '../data/hyde/'
 
 for file in os.listdir(directory):
     start = time.time()
-    header, data = rpg_cython.read_rpg(os.path.join(directory, file))
+    header, data = rpgpy.read_rpg(os.path.join(directory, file))
     end = time.time()
     print(end-start)
