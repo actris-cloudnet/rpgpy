643d81bd178e0b6184d88f05b54a2b2138decc75
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-12-08 13:12:09 +0200

    Fix RPG date conversion

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,5 +1,6 @@
 from rpgpy import utils
 from numpy.testing import assert_array_equal
+import pytest
 
 
 def test_rpg_seconds2date():
@@ -8,3 +9,13 @@ def test_rpg_seconds2date():
     res = ['2001', '01', '01', '00', '00', '00']
     assert_array_equal(date, res)
     assert_array_equal(date_only, res[:3])
+
+
+@pytest.mark.parametrize("input, result", [
+    (0, ['2001', '01', '01', '00', '00', '00']),
+    (24*60*60*10 + 1, ['2001', '01', '11', '00', '00', '01']),
+    (24*60*60 - 1, ['2001', '01', '01', '23', '59', '59']),
+    (625107602, ['2020', '10', '23', '01', '00', '02'])
+])
+def test_seconds2date(input, result):
+    assert utils.rpg_seconds2date(input) == result
