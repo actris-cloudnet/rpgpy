44ecefc4b75aad42ac34642facabfabe82ab106a
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-04-12 11:59:17 +0300

    Move package information to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from Cython.Build import cythonize
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
 
 extensions = [
     Extension(
@@ -8,43 +8,4 @@ extensions = [
     ),
 ]
 
-version: dict = {}
-with open("rpgpy/version.py", encoding="utf-8") as f:
-    exec(f.read(), version)  # pylint: disable=W0122
-
-with open("README.md", encoding="utf-8") as f:
-    readme = f.read()
-
-setup(
-    name="rpgPy",
-    description="Cython code for reading binary files from RPG cloud radar.",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="Simo Tukiainen",
-    author_email="simo.tukiainen@fmi.fi",
-    version=version["__version__"],
-    url="https://github.com/actris-cloudnet/rpgpy",
-    license="MIT License",
-    install_requires=["numpy", "cython", "netCDF4", "tqdm", "numba"],
-    extras_require={
-        "test": [
-            "pytest-flakefinder",
-            "pylint",
-            "mypy",
-        ],
-        "dev": ["pre-commit"],
-    },
-    include_package_data=True,
-    packages=find_packages(),
-    python_requires=">=3.8",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-    ],
-    ext_modules=cythonize(extensions, language_level=3),
-)
+setup(ext_modules=cythonize(extensions, language_level=3))
